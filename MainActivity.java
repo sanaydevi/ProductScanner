a73e44644f87845e777c1ec8affa7683359d331e
sanaydevi <sanaydevi@gmail.com>
2018-03-17 13:26:52 -0700

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -62,7 +62,7 @@ import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String CLOUD_VISION_API_KEY = "AIzaSyCJtf5WXsV9ofF-45Z5_vKD0pfgXzB4ULw";
+    private static final String CLOUD_VISION_API_KEY = "";//cannot include my API
     public static final String FILE_NAME = "temp.jpg";
     private static final String ANDROID_CERT_HEADER = "X-Android-Cert";
     private static final String ANDROID_PACKAGE_HEADER = "X-Android-Package";
